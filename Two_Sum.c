cafd1a696ef33f5ad671f82f898999fba36b8af1
python practice <40566142+jendy12345@users.noreply.github.com>
2023-10-23 10:53:30 +0800

    Update Two_Sum.c

--- Two_Sum.c
+++ Two_Sum.c
@@ -1,15 +1,19 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    *returnSize=2;
-    int *ret=(int*)malloc(2*sizeof(int)),i,j,temp;
-    for(i=0;i<numsSize;i++){
-        temp=target-nums[i];
-        for(j=i+1;j<numsSize;j++){
-            if(temp==nums[j]){
-                ret[0]=i;
-                ret[1]=j;
-                return ret;
+    *returnSize = 2;// 將所指向的變數數值為2，表示返回的結果會包含兩個index
+    int *ret = (int*)malloc(2*sizeof(int)),i,j,temp;   
+    //用ret定義一個整數指標，並使用malloc分配可以存放兩個整數的記憶體空間
+    for(i=0;i<numsSize;i++){    // 循環整個numsSize的長度
+        temp = target - nums[i]; // 計算目標和target-nums[i]得到的差值，並將變數存在temp中
+        for(j=i+1;j<numsSize;j++){  // 是內部循環，找到所有nums數組中i的元素
+            if(temp == nums[j]){    //判斷temp是否等於num[j]，如果相等，兩個數字相加為target的數字，則會將index個別存在ret[0](第一個位置)，ret[1](第二個位置)
+                ret[0] = i;
+                ret[1] = j;
+                return ret; // 返回ret,這輛個數組中存放了兩個數字的index
             }
         }
     }
-    return ret;
+    return ret; // 如果沒有找到符合的數字，直接返回ret
 }
