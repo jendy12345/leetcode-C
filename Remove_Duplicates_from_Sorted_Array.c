2d4f7a77548358a88565c98475ff61e04b9d13ea
python practice <40566142+jendy12345@users.noreply.github.com>
2023-10-31 16:18:33 +0800

    Update Remove_Duplicates_from_Sorted_Array.c

--- Remove_Duplicates_from_Sorted_Array.c
+++ Remove_Duplicates_from_Sorted_Array.c
@@ -7,3 +7,31 @@ int removeDuplicates(int* nums, int numsSize){
     }
     return index;
 }
+// new code
+#include<stdio.h>
+#include<stdlib.h>
+
+int removeDuplicates(int* nums, int numsSize){
+int index = 0;
+for(int i = 0; i < numsSize; i++){
+    if(i==0 || nums[i] > nums[i-1]){
+        nums[index++] = nums[i];
+    }
+}
+return index;
+}
+
+int main(){
+    int numSize = 10;
+    int nums[] = {0,0,1,1,1,2,2,3,3,4};
+    int result = removeDuplicates(nums,numSize);
+    printf("output： %d,nums=[",result);
+    for(int i=0;i<result;i++){
+        printf("%d",nums[i]);
+        if(i < result - 1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    return 0;
+}
