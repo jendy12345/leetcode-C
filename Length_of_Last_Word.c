9e8883622c26e37d8f119d72a7f78c16515d4038
python practice <40566142+jendy12345@users.noreply.github.com>
2023-11-07 10:10:15 +0800

    Update Length_of_Last_Word.c

--- Length_of_Last_Word.c
+++ Length_of_Last_Word.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 int lengthOfLastWord(char * s){
-    int count = 0;
-    for(int i = strlen(s)-1; i>=0; i--){
+    int count = 0;  // 使用count來記錄數字長度
+    for(int i = strlen(s)-1; i>=0; i--){    // 找到迴圈的最後一個字串並往前尋找，直到有找到非字元或是空格則count+1。
         if(s[i] != ' '){
-            count++;
+            count++;    // 如果找到非空格字元，則count+1
         }
-        else if(s[i]== ' ' && count != 0){
+        else if(s[i]== ' ' && count != 0){  // 如果找到空格字串且為零，則跳出迴圈。
             break;
         }
     }
